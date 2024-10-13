07bb2da17b63710eed7807a4c3fe240307935dbb
muhammadImtiaz27 <123411742+muhammadImtiaz27@users.noreply.github.com>
2024-10-13 16:37:56 +0800

    Update script.js
    Removed duplicated code.

--- script.js
+++ script.js
@@ -6,14 +6,6 @@ const formTennant = document.getElementById("formTennant");
 const inputICNumber = document.getElementById("inputICNumber");
 const btnSubmit = document.getElementById("btnSubmit");
 
-const video = document.getElementById("video");
-const btnTakePicture = document.getElementById("btnTakePicture");
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-const formTennant = document.getElementById("formTennant");
-const inputICNumber = document.getElementById("inputICNumber");
-const btnSubmit = document.getElementById("btnSubmit");
-
 let tennantICNumber, tennantImage, tennantLatitude, tennantLongitude;
 
 function geoLocationSuccess(position) {
