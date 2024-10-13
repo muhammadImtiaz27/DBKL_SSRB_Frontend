5aeb009ca8582c8a3bedc4792bd4d099326e6faa
muhammadImtiaz27 <123411742+muhammadImtiaz27@users.noreply.github.com>
2024-10-13 16:16:46 +0800

    Add starter files

---
+++ script.js
@@ -0,0 +1,7 @@
+const video = document.getElementById("video");
+const btnTakePicture = document.getElementById("btnTakePicture");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const formTennant = document.getElementById("formTennant");
+const inputICNumber = document.getElementById("inputICNumber");
+const btnSubmit = document.getElementById("btnSubmit");
